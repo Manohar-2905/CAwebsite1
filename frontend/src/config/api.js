552791ed184ac570fd30a38228bb86ac0796887a
Premrajsingh <premrajsingh@Premrajsinghs-MacBook-Air.local>
2026-01-19 15:10:12 +0530

    Remove all files from GitHub

--- frontend/src/config/api.js
+++
@@ -1,7 +0,0 @@
-const isLocal = window.location.hostname === "localhost" || window.location.hostname === "127.0.0.1";
-
-const API_BASE_URL = isLocal 
-  ? "http://localhost:5000/api" 
-  : (process.env.REACT_APP_API_URL || "https://caweb.onrender.com/api");
-
-export default API_BASE_URL;
